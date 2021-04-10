3b6b87062e048a7fac35448701e04b602873dffc
ycaoma <76047039+ycaoma@users.noreply.github.com>
2021-04-10 13:58:13 +0800

    Delete knockoffs_matlab directory

--- knockoffs_matlab/gen_knockoff/gen_knockoff.m
+++
@@ -1,6 +0,0 @@
-function[result] = gen_knockoff(X, D, y, q, method)
-    % method = "knockoff" or "knockoff+"
-    % convert the problem to regression
-    [X_new, y_new] = Transfer_design(X, D, y);
-    [result, ~] = knockoffs.filter(X_new, y_new, q, {'fixed'}, 'Method', 'equi', 'Threshold', method);
-end
--- knockoffs_matlab/split_knockoff/split_knockoff_original.m
+++
@@ -1,6 +0,0 @@
-function[result, Z, t_Z] = split_knockoff_original(X, D, y, nu, q, s_size, method)
-    % record Z, tilde_Z as well as W for further calculation
-    [Z, t_Z, W] = W_generate_original(X, D, y, nu, s_size);
-    % selection on W
-    result = knockoffs.select(W, q, method);
-end
